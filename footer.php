928c0596f1d8ade6e974fdf8925e3a71507006dc
digitalpandit11 <99058279+digitalpandit11@users.noreply.github.com>
2024-05-12 14:11:44 +0530

    footer css

--- footer.php
+++ footer.php
@@ -1,36 +1,54 @@
 <footer id="footer" class="footer bg-overlay">
-			<div class="footer-main">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-4 col-sm-12 footer-widget footer-about">
-							<h3 class="widget-title">About Us</h3>
-							<p class="jt">VB Digitech is a Top-ranked Website Development And Digital Marketing Company that serves in Pune, Mumbai, Nagpur, Thane, Bangalore and across India. VB Digitech was formed in 2015, and we are based in Pune, India.</p>
-							<div class="footer-social">
-								<ul>
-									<li><a href="https://www.facebook.com/VB.Digitech" target="_blank"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="https://twitter.com/DigitechVb" target="_blank"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="https://www.instagram.com/vbdigitech/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-									<li><a href="https://www.linkedin.com/company/vb-digitech-website-development-digital-marketing-services/about/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-								</ul>
-							</div><!-- Footer social end -->
-						</div><!-- Col end -->
+	<div class="footer-main">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-12 footer-widget footer-about">
+					<h3 class="widget-title">About Us</h3>
+					<p class="jt">VB Digitech is a Top-ranked Website Development And Digital Marketing Company that serves in Pune, Mumbai, Nagpur, Thane, Bangalore and across India. VB Digitech was formed in 2015, and we are based in Pune, India.</p>
+					<div class="footer-social">
+						<ul>
+							<li><a href="https://www.facebook.com/VB.Digitech" target="_blank"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="https://twitter.com/DigitechVb" target="_blank"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="https://www.instagram.com/vbdigitech/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+							<li><a href="https://www.linkedin.com/company/vb-digitech-website-development-digital-marketing-services/about/" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+						</ul>
+					</div><!-- Footer social end -->
+				</div><!-- Col end -->
 
-             <div class="col-md-4 col-sm-12 footer-widget">
-            <h3 class="widget-title">Contact Us</h3>
-            <div class="working-hours">
-              Visit Our Office: 
-              <span class="text-right">Narhe,Pune(MH),India. </span>
-              <br> 
-              Call Us:
-               <span class="text-right">(+91) 98225 58876&nbsp;&nbsp; &nbsp; &nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;</span>
-              <br> 
-               Email Us:
-               <span class="text-right">amol@vbdigitech.com &nbsp;&nbsp; &nbsp; &nbsp;&nbsp;</span>
-            </div>
-          </div><!-- Col end -->
+				<div class="col-md-4 col-sm-12 footer-widget">
+					<h3 class="widget-title">Contact Us</h3>
+
+					<div class="working-hours">
+						<div class="col-md-4 col-sm-4">
+							Office:
+						</div>
+						<div class="col-md-6 col-sm-6">
+							Narhe,Pune(MH),India.
+						</div>
+					</div>
+
+					<div class="working-hours">
+
+						<div class="col-md-4 col-sm-4">
+							Call Us:
+						</div>
+						<div class="col-md-6 col-sm-6">
+							(+91) 98225 58876
+						</div>
+					</div>
+
+					<div class="working-hours">
+						<div class="col-md-4 col-sm-4">
+							Email Us:
+						</div>
+						<div class="col-md-6 col-sm-6">
+							amol@vbdigitech.com
+						</div>
+					</div>
+				</div><!-- Col end -->
 
 
-					   <!--<div class="col-md-4 col-sm-12 footer-widget">
+				<!--<div class="col-md-4 col-sm-12 footer-widget">
 							<h3 class="widget-title">Contact Us</h3>
 						    <div class="working-hours">
 								Visit Our Office:<span class="text-right">Narhe,Pune(MH),India. </span><br>
@@ -38,36 +56,38 @@
 							      Email Us: <span class="text-right">   info@vbdigitech.com</span><br>
 						   </div>
 					  </diV>-->
-					<!-- Col end -->
+				<!-- Col end -->
 
-						<div class="col-md-4 col-sm-12 footer-widget">
-							<h3 class="widget-title">Sitemap</h3>
-							<ul class="list-arrow">
-								<li><a href="website-development.php">Website Development</a></li>
-								<li><a href="digital-marketing.php">Digital Marketing</a></li>
-								<li><a href="custom-software.php">Custom Software</a></li>
-								<li><a href="blog-sidebar.php">Blog</a></li>
+				<div class="col-md-4 col-sm-12 footer-widget">
+					<h3 class="widget-title">Sitemap</h3>
+					<ul class="list-arrow">
+						<li><a href="website-development.php">Website Development</a></li>
+						<li><a href="digital-marketing.php">Digital Marketing</a></li>
+						<li><a href="custom-software.php">Custom Software</a></li>
+						<li><a href="blog-sidebar.php">Blog</a></li>
 
-								<li><a href="https://vbdigitech.com/user_frontend_blogs/" target="_blank">Add New Blogs</a></li>
-							</ul>
-						</div><!-- Col end -->
+						<!-- <li><a href="https://vbdigitech.com/user_frontend_blogs/" target="_blank">Add New Blogs</a></li> -->
+					</ul>
+				</div><!-- Col end -->
 
 
-					</div><!-- Row end -->
-				</div><!-- Container end -->
-			</div><!-- Footer main end -->
+			</div><!-- Row end -->
+		</div><!-- Container end -->
+	</div><!-- Footer main end -->
 
-			<div class="copyright">
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-12 col-sm-6">
-							<div class="copyright-info">
-								<span>Copyright © <script>document.write(new Date().getFullYear())</script> Developed & Promoted with ❤️ by <br><a href="https://www.vbdigitech.com" target="_blank"><b>VB Digitech-Software Dvelopment & Digital Marketing Service Provider</a></span>
-							</div>
-						</div>
+	<div class="copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-sm-6">
+					<div class="copyright-info">
+						<span>Copyright © <script>
+								document.write(new Date().getFullYear())
+							</script> Developed & Promoted with ❤️ by <br><a href="https://www.vbdigitech.com" target="_blank"><b>VB Digitech- ERP, CRM , Websites, SEO, Google Ads</a></span>
+					</div>
+				</div>
 
-						
-						<!--<div class="col-xs-12 col-sm-6">
+
+				<!--<div class="col-xs-12 col-sm-6">
 							<div class="footer-menu">
 								<ul class="nav unstyled">
 									<li><a href="about.php">About</a></li>
@@ -78,40 +98,40 @@
 								</ul>
 							</div>
 						</div>-->
-					</div><!-- Row end -->
+			</div><!-- Row end -->
 
-					<div id="back-to-top" data-spy="affix" data-offset-top="10" class="back-to-top affix">
-						<button class="btn btn-primary" title="Back to Top">
-							<i class="fa fa-angle-double-up"></i>
-						</button>
-					</div>
+			<div id="back-to-top" data-spy="affix" data-offset-top="10" class="back-to-top affix">
+				<button class="btn btn-primary" title="Back to Top">
+					<i class="fa fa-angle-double-up"></i>
+				</button>
+			</div>
 
-				</div><!-- Container end -->
-			</div><!-- Copyright end -->
+		</div><!-- Container end -->
+	</div><!-- Copyright end -->
 
-		</footer><!-- Footer end -->
+</footer><!-- Footer end -->
 
 
-		<!-- initialize jQuery Library -->
-		<script type="text/javascript" src="js/jquery.js"></script>
-		<!-- Bootstrap jQuery -->
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
-		<!-- Owl Carousel -->
-		<script type="text/javascript" src="js/owl.carousel.min.js"></script>
-		<!-- Color box -->
-		<script type="text/javascript" src="js/jquery.colorbox.js"></script>
-		<!-- Isotope -->
-		<script type="text/javascript" src="js/isotope.js"></script>
-		<script type="text/javascript" src="js/ini.isotope.js"></script>
+<!-- initialize jQuery Library -->
+<script type="text/javascript" src="js/jquery.js"></script>
+<!-- Bootstrap jQuery -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- Owl Carousel -->
+<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+<!-- Color box -->
+<script type="text/javascript" src="js/jquery.colorbox.js"></script>
+<!-- Isotope -->
+<script type="text/javascript" src="js/isotope.js"></script>
+<script type="text/javascript" src="js/ini.isotope.js"></script>
 
-		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU&libraries=places"></script>
-		<!-- Google Map Plugin-->
-		<script type="text/javascript" src="js/gmap3.js"></script>
- 
-	 <!-- Template custom -->
-	 <script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCcABaamniA6OL5YvYSpB3pFMNrXwXnLwU&libraries=places"></script>
+<!-- Google Map Plugin-->
+<script type="text/javascript" src="js/gmap3.js"></script>
 
-	</div>
+<!-- Template custom -->
+<script type="text/javascript" src="js/custom.js"></script>
+
+</div>
 
 <!-- <script type="text/javascript">!function(t,e){t.artibotApi={l:[],t:[],on:function(){this.l.push(arguments)},trigger:function(){this.t.push(arguments)}};var a=!1,i=e.createElement("script");i.async=!0,i.type="text/javascript",i.src="https://app.artibot.ai/loader.js",e.getElementsByTagName("head").item(0).appendChild(i),i.onreadystatechange=i.onload=function(){if(!(a||this.readyState&&"loaded"!=this.readyState&&"complete"!=this.readyState)){new window.ArtiBot({i:"d5bcc1c9-643f-4e43-9416-f776e2cb895a"});a=!0}}}(window,document);</script> -->
 
